7a5a7e22c40a10902e6de6cd89936980f8c84e3f
Skylar A <aderhold@umich.edu>
2023-05-31 18:12:23 -0400

    Initial Site

---
+++ TheLegacyVoyage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheLegacyVoyage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
